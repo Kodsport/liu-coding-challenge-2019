1bba324ec02d0cd921cb178ebe47ad611e786c34
Johan Sannemo <jsannemo@google.com>
2019-03-28 22:30:06 +0100

    Add WA solution for baas

---
+++ baas/submissions/wrong_answer/sl2.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+	cin.sync_with_stdio(0); cin.tie(0);
+	cin.exceptions(cin.failbit);
+	int N;
+	cin >> N;
+	vector<vi> deps(N);
+	vector<ll> times(N);
+	rep(i,0,N) cin >> times[i];
+	rep(i,0,N) {
+		int count;
+		cin >> count;
+		deps[i].resize(count);
+		rep(j,0,count) {
+			cin >> deps[i][j];
+			--deps[i][j];
+			assert(deps[i][j] < i);
+		}
+	}
+
+  ll mxtime = *max_element(all(times));
+
+	ll res = LLONG_MAX;
+	rep(elim,0,N) {
+    if (times[elim] != mxtime) continue;
+		vector<ll> dp(N);
+		rep(i,0,N) {
+			ll start = 0;
+			trav(x, deps[i]) start = max(start, dp[x]);
+			dp[i] = start + (elim == i ? 0 : times[i]);
+		}
+		res = min(res, dp.back());
+	}
+	cout << res << endl;
+}
